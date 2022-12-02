0d86cbad111e15ce64546527ab00cdbff439c3ce
Alvaro Javier <alvarojavierchagas@hotmail.com>
2022-12-02 19:51:57 +0000

    Funcionando planetas, personasrompe la app

--- src/front/js/pages/people.jsx
+++ src/front/js/pages/people.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 
 export const People = () => {
 	const { store, actions } = useContext(Context);
-	useEffect (() => {
+	useEffect(() => {
 
 	}, [])
 
@@ -13,15 +12,15 @@ export const People = () => {
 		<div className="container text-center mt-5">
 			<h1>People</h1>
 			<button className="btn btn-success" onClick={() => actions.getPeople()}>
-            Cargar Personas
-        </button>
-        <ul>
-            {store.people.map((people, index) =>(
-                <li key={index}>{people.name}</li>
-            ))}
-        </ul>
+				Cargar Personas
+			</button>
+			<ul>
+				{store.people.map((people, index) => (
+					<li key={index}>{people.name}</li>
+				))}
+			</ul>
 		</div>
-		
+
 	);
 };
 
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,30 +1,28 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			planets: [],
-			people: []
-		},
-		actions: {
-			getPlanets: async () => {
-				let resp = await fetch ("https://swapi.dev/api/planets/")
-				let data = await resp.json()
-				setStore({
-					...getStore(),
-					planets: data.results
-				})
-			}
-		},
-		getPeople: async () => {
-			let resp = await fetch ("https://swapi.dev/api/people/")
-			let data = await resp.json()
-			setStore({
-				...getStore(),
-				people: data.results
-			})
-		}
-		
-	
-	};
-}
+  return {
+    store: {
+      planets: [],
+      people: [],
+    },
+    actions: {
+      getPlanets: async () => {
+        let resp = await fetch("https://swapi.tech/api/planets/");
+        let data = await resp.json();
+        setStore({
+          ...getStore(),
+          planets: data.results,
+        });
+      },
+    },
+    getPeople: async () => {
+      let resp = await fetch("https://swapi.tech/api/people/");
+      let data = await resp.json();
+      setStore({
+        ...getStore(),
+        people: data.results,
+      });
+    },
+  };
+};
 
 export default getState;
