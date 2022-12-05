1dee6d7817684fc526df5c268bc71e4273e7278e
Alvaro Javier <alvarojavierchagas@hotmail.com>
2022-12-05 16:58:39 +0000

    Modificando el home

--- src/front/js/pages/home.jsx
+++ src/front/js/pages/home.jsx
@@ -2,16 +2,42 @@ import React, { useState, useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 import { Link, useParams } from "react-router-dom";
+import CardList from "../component/cardList.jsx";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 	const params = useParams();
-	useEffect (() => {}, [])
+	const [links, setLinks] = useState([
+		{ text: "Planets", link: "/planets" },
+		{ text: "Films", link: "/films" },
+		{ text: "People", link: "/people" },
+		{ text: "Species", link: "/species" },
+		{ text: "Starships", link: "/starships" },
+		{ text: "Vehicles", link: "/vehicles" }
+	])
+
+	useEffect(() => { actions.getStarWars() }, [])
+
 
 	return (
 		<div className="container">
 			<h1 className="m-3">STAR WARS</h1>
-			
+			<div className="container">
+				<div className="row">
+					<div class="card">
+						<ul>
+							{
+								links.map((link, index) => (
+									<li key={index} className="nav-item">
+										<Link className="nav-link" to={link.link}>{link.text}</Link>
+									</li>
+								))
+							}
+						</ul>
+
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
