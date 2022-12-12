865d59a4a442b28bf01a9a8923b1d1b70069d464
Alvaro Javier <alvarojavierchagas@hotmail.com>
2022-12-12 17:16:27 +0000

    Problemas con el home

--- src/front/js/pages/home.jsx
+++ src/front/js/pages/home.jsx
@@ -1,30 +1,20 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 import { Link, useParams } from "react-router-dom";
 import CardList from "../component/cardList.jsx";
-
 export const Home = () => {
-
-	const [store, actions] = useContext(Context)
-
+	const { store, actions } = useContext(Context);
+	const params = useParams();
+	
+	useEffect(() => { actions.getStarWars() }, [])
 	return (
 		<div className="container">
 			<h1 className="m-3">STAR WARS</h1>
 			<div className="container">
 				<div className="row">
-					<ul className="list-group-horizontal">
-						<li className="list-group-item">
-							<CardList
-								id={1}
-								type="planets"
-								title="Dagoba"
-								text="A Star Wars Planet"
-								img={`https://starwars-visualguide.com/assets/img/planets/1.jpg`}
-							/>
-						</li>
-					</ul>
-				</div>
+					
+			</div>
 			</div>
 		</div>
 	);
