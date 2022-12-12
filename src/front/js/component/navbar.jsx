81d1daf8b7dd779af6fde5289878e7bd668ca614
Alvaro Javier <alvarojavierchagas@hotmail.com>
2022-12-12 20:44:06 +0000

    Casi pronto el home

--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -1,15 +1,17 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   const [links, setLinks] = useState([
-    {text: "Planets", link: "/planets"},
-    {text: "Films", link: "/films"},
-    {text: "People", link: "/people"},
-    {text: "Species", link: "/species"},
-    {text: "Starships", link: "/starships"},
-    {text: "Vehicles", link: "/vehicles"}
+    { text: "Planets", link: "/planets" },
+    { text: "Films", link: "/films" },
+    { text: "People", link: "/people" },
+    { text: "Species", link: "/species" },
+    { text: "Starships", link: "/starships" },
+    { text: "Vehicles", link: "/vehicles" }
   ])
+
+
   return (
     <nav className="navbar navbar-expand-md navbar-light bg-secondary">
       <div className="container-fluid">
@@ -22,16 +24,17 @@ export const Navbar = () => {
             {
               links.map((link, index) => (
                 <li key={index} className="nav-item">
-              <Link className="nav-link" to = {link.link}>{link.text}</Link>
-            </li>
+                  <Link className="nav-link" to={link.link}>{link.text}</Link>
+                </li>
               ))
             }
             <li className="nav-item dropdown ml-auto">
               <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown link
+                Favorites
               </a>
               <ul className="dropdown-menu bg-secondary" aria-labelledby="navbarDropdownMenuLink">
-                <li><a className="dropdown-item" href="#">Action</a></li>
+                
+                <li><a className="dropdown-item" href="#">FAV1</a></li>
                 <li><a className="dropdown-item" href="#">Another action</a></li>
                 <li><a className="dropdown-item" href="#">Something else here</a></li>
               </ul>
--- src/front/js/pages/home.jsx
+++ src/front/js/pages/home.jsx
@@ -6,16 +6,57 @@ import CardList from "../component/cardList.jsx";
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 	const params = useParams();
-	
-	useEffect(() => { actions.getStarWars() }, [])
+	const [pages, setPages] = useState(0)
+	const [records, setRecords] = useState(0)
+
+	useEffect(() => {
+		actions.getStarWars("people", "planets").then((resp) => {
+			if (resp) {
+				setPages(resp.pages)
+				setRecords(resp.records)
+			}
+		})
+	}, [])
+
 	return (
-		<div className="container">
+		<div className="text-center-mt-5">
 			<h1 className="m-3">STAR WARS</h1>
-			<div className="container">
-			<div className="row">
-					
-				</div>
+			
+			<h5>People</h5>
+
+			<ul className="list-group-horizontal overflow-scroll d-flex"
+				style={{ overflowY: "hidden !important" }}>
+				{store.people.map((people, index) => (
+					<li key={people.uid} className="col col-md-4">
+						<CardList
+							id={people.uid}
+							type="people"
+							title={people.name}
+							text={people.description}
+							img={`https://starwars-visualguide.com/assets/img/characters/${people.uid}.jpg`}
+						/>
+					</li>
+				))}
+			</ul>
+
+
+			<h5 className="mt-3">Planets</h5>
+
+			<ul className="list-group-horizontal overflow-scroll d-flex">
+				{store.planets.map((planet, index) => (
+					<li key={planet.uid} className="col col-md-4">
+						<CardList
+							id={planet.uid}
+							type="planets"
+							title={planet.name}
+							text={planet.description}
+							img={`https://starwars-visualguide.com/assets/img/planets/${planet.uid}.jpg`}
+						/>
+					</li>
+				))}
+
+			</ul>
 			</div>
-		</div>
-	);
+
+			);
 };
