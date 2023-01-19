d83875a14d8a276d6415c4d464bc271f0778826a
Alvaro Javier <alvarojavierchagas@hotmail.com>
2023-01-19 20:47:05 +0000

    Link a home

--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import starImage from "../../img/starwars.png";
 
 export const Navbar = () => {
   const [links, setLinks] = useState([
@@ -15,7 +16,10 @@ export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-md navbar-light bg-secondary">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Star Wars</Link>
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">
+            <img src={starImage} width="80" height="50" /></span>
+        </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -33,7 +37,7 @@ export const Navbar = () => {
                 Favorites
               </a>
               <ul className="dropdown-menu bg-secondary" aria-labelledby="navbarDropdownMenuLink">
-                
+
                 <li><a className="dropdown-item" href="#">FAV1</a></li>
                 <li><a className="dropdown-item" href="#">Another action</a></li>
                 <li><a className="dropdown-item" href="#">Something else here</a></li>
